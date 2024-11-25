9701dc92548788e225e82bc50bb2f70d3f381a34
suffer-sami <sami_email@protonmail.com>
2024-11-25 09:31:47 +0500

    add cfg

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	jwtSecret string
+	client    *http.Client
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	godotenv.Load()
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		log.Fatal("JWT_SECRET must be set")
+	}
+
+	client := &http.Client{}
+
+	cfg := &Config{
+		jwtSecret: jwtSecret,
+		client:    client,
+	}
 }
