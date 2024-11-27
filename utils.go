c3174e3681bf7ec2b927b05833b78dfeb0ca92b3
suffer-sami <sami_email@protonmail.com>
2024-11-28 01:15:32 +0500

    remove unused func

--- utils.go
+++ utils.go
@@ -15,13 +15,8 @@ func toNullInt(i int) sql.NullInt64 {
 	return sql.NullInt64{Int64: int64(i), Valid: i != 0}
 }
 
-// Converts an int64 to sql.NullInt64
-func toNullInt64(i int64) sql.NullInt64 {
-	return sql.NullInt64{Int64: i, Valid: i != 0}
-}
-
 // Converts a string w/ layout to sql.NullTime
-func toStrNullTime(t string, layout string) sql.NullTime {
+func strToNullTime(t string, layout string) sql.NullTime {
 	parsedTime, err := time.Parse(layout, t)
 	if err != nil || t == "" {
 		return sql.NullTime{Valid: false}
