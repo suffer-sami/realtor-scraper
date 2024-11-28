9d357ab1beb10827b080e6d923f7b0ec7dfc1121
suffer-sami <sami_email@protonmail.com>
2024-11-28 21:59:12 +0500

    add more conversion func

--- utils.go
+++ utils.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"database/sql"
+	"strings"
 	"time"
 )
 
 // Converts a string to sql.NullString
 func toNullString(s string) sql.NullString {
+	s = strings.TrimSpace(s)
 	return sql.NullString{String: s, Valid: s != ""}
 }
 
 // Converts an int to sql.NullInt64
 func toNullInt(i int) sql.NullInt64 {
 	return sql.NullInt64{Int64: int64(i), Valid: i != 0}
+}
+
+// Converts an int64 to sql.NullInt64
+func toNullInt64(i int64) sql.NullInt64 {
+	return sql.NullInt64{Int64: i, Valid: i != 0}
+}
+
+// Converts a bool to sql.NullBool
+func toNullBool(b bool) sql.NullBool {
+	return sql.NullBool{Bool: b, Valid: true}
+}
+
+// Converts an numeric type to sql.NullInt64
+func numericToNullInt(i NumericType) sql.NullInt64 {
+	return sql.NullInt64{Int64: int64(i), Valid: i != 0}
+}
+
+// Converts time.Time to sql.NullTime
+func timeToNullTime(t time.Time) sql.NullTime {
+	if t.IsZero() {
+		return sql.NullTime{Valid: false}
+	}
+	return sql.NullTime{Time: t, Valid: true}
 }
 
 // Converts a string w/ layout to sql.NullTime
