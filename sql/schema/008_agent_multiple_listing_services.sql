0237116b63d8728bd101152bbbaa9d21fcbcf300
suffer-sami <sami_email@protonmail.com>
2024-11-28 18:23:46 +0500

    use explicit constraints

--- sql/schema/008_agent_multiple_listing_services.sql
+++ sql/schema/008_agent_multiple_listing_services.sql
@@ -1,7 +1,15 @@
 -- +goose Up
 CREATE TABLE agent_multiple_listing_services (
-    agent_id TEXT REFERENCES agents(id) ON DELETE CASCADE,
-    multiple_listing_service_id INTEGER REFERENCES multiple_listing_services(id) ON DELETE CASCADE,
+    agent_id TEXT,
+    multiple_listing_service_id INTEGER,
+    CONSTRAINT fk_agents
+        FOREIGN KEY (agent_id)
+        REFERENCES agents(id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_multiple_listing_services
+        FOREIGN KEY (multiple_listing_service_id)
+        REFERENCES multiple_listing_services(id)
+        ON DELETE CASCADE,
     PRIMARY KEY (agent_id, multiple_listing_service_id)
 );
 
--- sql/schema/010_agent_languages.sql
+++ sql/schema/010_agent_languages.sql
@@ -1,7 +1,15 @@
 -- +goose Up
 CREATE TABLE agent_languages (
-    agent_id TEXT REFERENCES agents(id) ON DELETE CASCADE,
-    language_id INTEGER REFERENCES languages(id),
+    agent_id TEXT,
+    language_id INTEGER,
+    CONSTRAINT fk_agents
+        FOREIGN KEY (agent_id)
+        REFERENCES agents(id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_languages
+        FOREIGN KEY (language_id)
+        REFERENCES languages(id)
+        ON DELETE CASCADE,
     PRIMARY KEY (agent_id, language_id)
 );
 
